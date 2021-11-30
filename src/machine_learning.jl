64208070df8824a74957b37d6c31ed8cf538945b
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-11-30 17:14:19 +0800

    machine learning using Julia

---
+++ src/machine_learning.jl
@@ -0,0 +1,10 @@
+module machine_learning
+
+using FastAI
+data, blocks = loaddataset("imagenette2-160", (Image, Label))
+method = ImageClassificationSingle(blocks)
+learner = methodlearner(method, data, callbacks=[ToGPU()])
+fitonecycle!(learner, 10)
+showoutputs(method, learner)
+
+end # module
