56875676afe2d2ea25d704947760ae2b0867ec1e
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-01 14:18:40 +0800

    predict simple function 4x+2

--- src/machine_learning.jl
+++ src/machine_learning.jl
@@ -1,3 +1,28 @@
 module machine_learning
 
+using Flux
+using Flux: train!
+
+actual(x) = 4x + 2
+
+x_train, x_test = hcat(0:5...), hcat(6:10...)
+y_train, y_test = actual.(x_train), actual.(x_test)
+
+# stands for 1 input and 1 output
+predict = Dense(1, 1)
+loss(x, y) = Flux.Losses.mse(predict(x), y)
+
+opt = Descent()
+data = [(x_train, y_train)]
+parameters = params(predict)
+println("before train")
+println("loss: $(loss(x_train, y_train))")
+println("parameters: $parameters")
+for epoch in 1:200
+  train!(loss, parameters, data, opt)
+end
+println("after train")
+println("loss: $(loss(x_train, y_train))")
+println("parameters: $parameters")
+
 end # module
