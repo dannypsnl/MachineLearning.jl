5a9ec313ad0f54ca44df219a05b868856f640f8f
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-05 10:50:15 +0800

    use DataLoader

--- src/machine_learning.jl
+++ src/machine_learning.jl
@@ -1,21 +1,20 @@
 using Flux
 using Flux: train!, @epochs
+using Flux: Data.DataLoader
 
 actual(x) = 4x + 2
 
 x_train, x_test = hcat(0:5...), hcat(6:10...)
 y_train, y_test = actual.(x_train), actual.(x_test)
+data = DataLoader((x_train, y_train), shuffle=true)
 
-# stands for 1 input and 1 output
-predict = Dense(1, 1)
-loss(x, y) = Flux.Losses.mse(predict(x), y)
+model = Dense(1, 1)
+loss(x, y) = Flux.Losses.mse(model(x), y)
 
 opt = Descent()
-data = [(x_train, y_train)]
-parameters = params(predict)
 
 @epochs 200 begin
-  train!(loss, parameters, data, opt)
-  @show(loss(x_train, y_train))
-  @show(parameters)
+  train!(loss, params(model), data, opt)
+  @show(loss(x_test, y_test))
+  @show(params(model))
 end
