360892a1f467a4592233a1ede079efb66ff3eb3f
dannypsnl <dannypsnl@users.noreply.github.com>
2021-12-11 08:44:42 +0000

    deploy: 08ae3b8b2fa562d02565a088c1777ac10f8550fe

---
+++ search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MachineLearning.jl","page":"MachineLearning.jl","title":"MachineLearning.jl","text":"","category":"section"},{"location":"","page":"MachineLearning.jl","title":"MachineLearning.jl","text":"Documentation for MachineLearning.jl","category":"page"}]
+}
